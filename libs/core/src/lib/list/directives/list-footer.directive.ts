7a64d2ed9949804afe819f8c9434f64491f1972b
Platon Rov <platon.rov@gmail.com>
2022-07-20 21:25:44 +0200

    fix(core): improvements of skeleton applying on core components

--- libs/core/src/lib/list/directives/list-footer.directive.ts
+++ libs/core/src/lib/list/directives/list-footer.directive.ts
@@ -1,10 +1,17 @@
 import { Directive, HostBinding } from '@angular/core';
+import { SkeletonConsumerDirective, skeletonConsumerProviders } from '@fundamental-ngx/core/skeleton';
 
 @Directive({
-    selector: '[fdListFooter], [fd-list-footer]'
+    selector: '[fdListFooter], [fd-list-footer]',
+    providers: skeletonConsumerProviders({ text: true })
 })
 export class ListFooterDirective {
     /** @hidden */
     @HostBinding('class.fd-list__footer')
     fdListFooterClass = true;
+
+    /** @hidden */
+    constructor(private readonly _skeletonConsumer: SkeletonConsumerDirective) {
+        _skeletonConsumer.consume();
+    }
 }
--- libs/core/src/lib/list/directives/list-secondary.directive.ts
+++ libs/core/src/lib/list/directives/list-secondary.directive.ts
@@ -1,10 +1,17 @@
 import { Directive, HostBinding } from '@angular/core';
+import { SkeletonConsumerDirective, skeletonConsumerProviders } from '@fundamental-ngx/core/skeleton';
 
 @Directive({
-    selector: '[fd-list-secondary] ,[fdListSecondary]'
+    selector: '[fd-list-secondary] ,[fdListSecondary]',
+    providers: skeletonConsumerProviders({ text: true })
 })
 export class ListSecondaryDirective {
     /** @hidden */
     @HostBinding('class.fd-list__secondary')
     fdListSecondaryClass = true;
+
+    /** @hidden */
+    constructor(private readonly _skeletonConsumer: SkeletonConsumerDirective) {
+        _skeletonConsumer.consume();
+    }
 }
