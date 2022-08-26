5fe1f06a063683e2594034fe2e599e552041ccc7
Platon Rov <platon.rov@gmail.com>
2022-08-26 13:38:49 +0200

    fix(core): skeleton for message page, pagination, product switch, tab list

--- libs/core/src/lib/message-page/message-page-subtitle/message-page-subtitle.component.ts
+++ libs/core/src/lib/message-page/message-page-subtitle/message-page-subtitle.component.ts
@@ -1,20 +1,13 @@
 import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation } from '@angular/core';
-import { skeletonConsumerProviders, SkeletonConsumerDirective } from '@fundamental-ngx/core/skeleton';
 
 @Component({
     selector: 'fd-message-page-subtitle',
     template: `<ng-content></ng-content>`,
     encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: skeletonConsumerProviders()
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagePageSubtitleComponent {
     /** @hidden */
     @HostBinding('class.fd-message-page__subtitle')
     fdMessagePageTitleClass = true;
-
-    /** @hidden */
-    constructor(private readonly _skeletonConsumer: SkeletonConsumerDirective) {
-        _skeletonConsumer.consume();
-    }
 }
--- libs/core/src/lib/message-page/message-page-title/message-page-title.component.ts
+++ libs/core/src/lib/message-page/message-page-title/message-page-title.component.ts
@@ -1,20 +1,13 @@
 import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation } from '@angular/core';
-import { SkeletonConsumerDirective, skeletonConsumerProviders } from '@fundamental-ngx/core/skeleton';
 
 @Component({
     selector: 'fd-message-page-title',
     template: `<ng-content></ng-content>`,
     encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: skeletonConsumerProviders()
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagePageTitleComponent {
     /** @hidden */
     @HostBinding('class.fd-message-page__title')
     fdMessagePageTitleClass = true;
-
-    /** @hidden */
-    constructor(private readonly _skeletonConsumer: SkeletonConsumerDirective) {
-        _skeletonConsumer.consume();
-    }
 }
